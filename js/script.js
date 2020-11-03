f05d16998a7e9ab11c96928b0171da24f0095294
Milon <nm.milon97@gmail.com>
2020-11-03 06:34:31 -0800

    Design by Milon

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 jQuery(document).ready(function () {
     "use strict";
+    $(window).load(function () {
+        $("#sticker").sticky({
+            topSpacing: 0
+        },1000);
+    });
       $(function(){
 		  new WOW().init(); 
 		});
-
+      
 
         // Mean Menu
         $('.mean-menu').meanmenu({
             meanScreenWidth: "991"
         });
-        // Header Sticky
-        $(window).on('scroll',function() {
-            if ($(this).scrollTop() > 120){  
-                $('.sticky-nav').addClass("is-sticky");
-            }
-            else{
-                $('.sticky-nav').removeClass("is-sticky");
-            }
-        });
-
+    
     // home text slider 
     $('.main-slider').owlCarousel({
         items: 1,
@@ -69,8 +65,70 @@ jQuery(document).ready(function () {
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
     });
 
+   // Portfolio Slide End
+
+   $('.testimonials').owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        autoplay: false,
+        nav: true,
+        autoplayHoverPause: true,
+        margin: 0,
+        smartSpeed: 1000,
+        autoplayTimeout: 5000,
+        mouseDrag: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    });
+
+   // Testimonials slider End
+   $('.logo-slider').owlCarousel({
+        items: 5,
+        loop: true,
+        dots: true,
+        autoplay: false,
+        nav: true,
+        autoplayHoverPause: true,
+        margin: 15,
+        smartSpeed: 1000,
+        autoplayTimeout: 5000,
+        mouseDrag: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsiveClass: true,
+                responsive: {
+                    300: {
+                        items: 1,
+                        nav: true,
+                    },
+                    480: {
+                        items: 2,
+                        nav: true,
+                    },
+                    768: {
+                        items: 3,
+                        nav: true,
+                    },
+                    1170: {
+                        items: 6,
+                         nav: true,
+                    },
+                }
+    });
+
+   // Client Logo End
+
+
+
+
+
+
+
+
+
+
+
    })
 
-// (jQuery);
+
 
 
